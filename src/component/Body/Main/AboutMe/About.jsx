ba946ac91632c9808d3de9735d9f17c09bca7a55
chhairongCoding <chhinchhairong18@gmail.com>
2024-07-06 02:07:34 +0700

    Update with about and change alitle effect

--- src/component/Body/Main/AboutMe/About.jsx
+++ src/component/Body/Main/AboutMe/About.jsx
@@ -1,11 +1,14 @@
 import "./About.css";
 import aboutImg from "./aboutImg/aboutImg.jpg";
+import Effect from "../../../Effect/Effect";
 const Main = () => {
   return (
     <div>
       <div className="about">
         <div className="boxImg">
-          <img src={aboutImg} alt="" />
+          <div className="backImg">
+            <img src={aboutImg} alt="" />
+          </div>
         </div>
         <div className="aboutContain">
           <h2>About me</h2>
@@ -13,7 +16,18 @@ const Main = () => {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
             hic quisquam inventore officiis voluptate veritatis excepturi
             laborum consectetur quidem exercitationem. Reiciendis vitae
-            voluptate saepe, rerum ipsa earum perspiciatis ut ea.
+            voluptate saepe, rerum ipsa earum perspiciatis ut ea. Lorem ipsum
+            dolor sit amet consectetur adipisicing elit. Molestias hic quisquam
+            inventore officiis voluptate veritatis excepturi laborum consectetur
+            quidem exercitationem. Reiciendis vitae voluptate saepe, rerum ipsa
+            earum perspiciatis ut ea.
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
+            officia molestias explicabo sunt voluptatibus nemo accusantium,
+            impedit laborum distinctio nihil, earum atque maxime amet mollitia
+            sit aliquid iure accusamus ducimus? Lorem ipsum dolor sit amet
+            consectetur adipisicing elit.
           </p>
         </div>
       </div>
